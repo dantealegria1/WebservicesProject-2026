6a8dde22670c5cc63fc799f54a455b8f1eb4b26e
Dante Alegria <dalegria@data-keeper.com>
2026-03-03 21:32:39 -0600

    cache

--- client/src/core/interceptors/loading-interceptor.ts
+++ client/src/core/interceptors/loading-interceptor.ts
@@ -1,18 +1,29 @@
-
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpEvent, HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { BusyService } from '../services/busy-service';
-import { delay, finalize } from 'rxjs';
+import { delay, finalize, of, tap } from 'rxjs';
+
+const cache = new Map<string, HttpEvent<unknown>>();
 
 export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
-  const busyService = inject(BusyService);
+const busyService = inject(BusyService);
 
-  busyService.busy();
+  if (req.method === 'GET') {
+    const cachedResponse = cache.get(req.url);
+    if (cachedResponse) {
+      return of(cachedResponse);
+    }
+  }
 
-  return next(req).pipe(
-    delay(500),
-    finalize(() => {
-      busyService.idle();
-    })
-  );
+busyService.busy();
+
+return next(req).pipe(
+delay(500),
+    tap(response => {
+      cache.set(req.url, response)
+    }),
+finalize(() => {
+busyService.idle();
+})
+);
 };
