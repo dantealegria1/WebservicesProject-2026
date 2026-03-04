848cf167f2a2c114b2befba344716bff17fa19a1
Dante Alegria <dalegria@data-keeper.com>
2026-03-03 21:44:46 -0600

    photo upload

--- src/API/Controllers/MembersController.cs
+++ src/API/Controllers/MembersController.cs
@@ -10,8 +10,9 @@ using API.DTOs;
 namespace API.Controllers;
 
 [Authorize]
-public class MembersController(IMembersRepository membersRepository) : BaseApiController
-{
+public class MembersController(IMembersRepository membersRepository,
+    IPhotoService photoService) : BaseApiController
+    {
     [HttpGet]
     public async Task<ActionResult<IReadOnlyList<Member>>> GetMembers()
     {
@@ -63,4 +64,44 @@ public class MembersController(IMembersRepository membersRepository) : BaseApiCo
 
         return BadRequest("Failed to update profile");
     } 
+
+    [HttpPost("photo")]
+    public async Task<ActionResult<Photo>> AddPhoto([FromForm] IFormFile file)
+    {
+        var member = await membersRepository.GetMemberForUpdateAsync(User.GetMemberId());
+
+        if (member == null)
+        {
+            return NotFound("Member not found");
+        }
+
+        var result = await photoService.UploadPhotoAsync(file);
+
+        if (result.Error != null)
+        {
+            return BadRequest(result.Error.Message);
+        }
+
+        var photo = new Photo
+        {
+            Url = result.SecureUrl.AbsoluteUri,
+            PublicId = result.PublicId,
+            MemberId = User.GetMemberId()
+        };
+
+        if (member.ImageUrl == null)
+        {
+            member.ImageUrl = photo.Url;
+            member.User.ImageUrl = photo.Url;
+        }
+
+        member.Photos.Add(photo);
+
+        if (await membersRepository.SaveAllAsync())
+        {
+            return photo;
+        }
+
+        return BadRequest("Somehting went wrong!");
+    }
 }
