5b5696996173918d93d565b3bb3a5ad9a1e57f0d
dantealegria1 <dante.romero18@gmail.com>
2026-04-12 17:19:09 -0600

    Parcial-02 2026-04-12 client/src/features/account/register/register.html,client/src/features/account/register/register.ts

--- client/src/features/account/register/register.ts
+++ client/src/features/account/register/register.ts
@@ -1,27 +1,51 @@
-import { Component, inject, input, output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, inject, OnInit, output } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AccountService } from '../../../core/services/account-service';
 import { RegisterCreds } from '../../../types/user';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-register',
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
-export class Register {
+export class Register implements OnInit {
   private accountService = inject(AccountService);
-  cancelRegister = output<boolean>();
   protected creds = {} as RegisterCreds;
+  protected registerForm: FormGroup = new FormGroup({});
+  cancelRegister = output<boolean>();
+
+  private readonly EMAIL_REGEX = '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$';
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.registerForm = new FormGroup({
+      email: new FormControl('',
+        [Validators.required, Validators.pattern(this.EMAIL_REGEX)]),
+      displayName: new FormControl('',
+        [Validators.required]),
+      password: new FormControl('',
+        [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
+      confirmPassword: new FormControl('',
+        [Validators.required])
+    })
+  }
 
   register(): void {
-    this.accountService.register(this.creds).subscribe({
-      next: response => {
-        console.log(response);
-        this.cancel();
-      },
-      error: error => console.log(error)
-    });
+    console.group("REGISTER");
+    console.log(this.registerForm.value);
+    console.groupEnd();
+    // this.accountService.register(this.creds).subscribe({
+    //   next: response => {
+    //     console.log(response);
+    //     this.cancel();
+    //   },
+    //   error: error => console.log(error)
+    // });
   }
 
   cancel(): void {
